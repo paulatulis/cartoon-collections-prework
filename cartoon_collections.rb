574e547628a9e8b980a3ab29646c225fb860dc68
Paula Tulis <tulis.paula@gmail.com>
2019-03-26 01:09:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each do 
     |name, index| 
-    index_plus_one = index + 1 
-    puts "#{index_plus_one}. #{name}"
+    puts "#{index.to_i+1}. #{name}"
 end
 end
 
